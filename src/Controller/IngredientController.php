cdb4d59672a0f4226c4ea7fda8036a107ba1344b
BEDIN <bedinadejosue39@gmail.com>
2024-03-04 04:43:50 +0000

    integration d'un paginator 👍

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,16 +3,23 @@
 namespace App\Controller;
 
 use App\Repository\IngrediantRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class IngredientController extends AbstractController
 {
     #[Route('/', name: 'app_ingredient')]
-    public function index(IngrediantRepository $repository): Response
+    public function index(IngrediantRepository $repository, PaginatorInterface $paginator ,Request $request): Response
     {
-        $ingredient = $repository->findAll();
+        $ingredient = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
         return $this->render('pages/ingredient/index.html.twig', [
             'Ingredient'=>$ingredient
         ]);
